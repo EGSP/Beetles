04cd60eb21800ac483b234cf0c105b31f44cbd57
EveryGameSPlay <repsbro@gmail.com>
2021-06-21 20:29:31 +0300

    Added Colony type.

---
+++ Assets/_ProjectFiles/Code/Ai/Colony/Colony.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using Game.Entities.Beetles;
+using Game.Environment.Buildings;
+using UnityEngine;
+
+namespace Game.Ai
+{
+    /// <summary>
+    /// Колония хранит информацию о принадлежащих ей объектах и обрабатывает их.
+    /// </summary>
+    public class Colony : MonoBehaviour
+    {
+        /// <summary>
+        /// Отношения к другим колониям.
+        /// </summary>
+        private List<Relation> _relations = new List<Relation>();
+
+        /// <summary>
+        /// Свои жуки.
+        /// </summary>
+        private List<Beetle> _beetles = new List<Beetle>();
+        
+        /// <summary>
+        /// Свои постройки.
+        /// </summary>
+        private List<Building> _buildings = new List<Building>();
+    }
+}
---
+++ Assets/_ProjectFiles/Code/Ai/Colony/Relation.cs
@@ -0,0 +1,52 @@
+﻿using Egsp.Core;
+
+namespace Game.Ai
+{
+    /// <summary>
+    /// Отношение к колонии от 0 до 100.
+    /// </summary>
+    public class Relation
+    {
+        private int _value;
+        
+        /// <summary>
+        /// Колония-носитель данного отношения.
+        /// </summary>
+        public NotNone<Colony> Owner { get; private set; }
+
+        /// <summary>
+        /// Колония, к которой применимо отношение.
+        /// </summary>
+        public NotNone<Colony> Colony { get; private set; }
+
+        /// <summary>
+        /// Числовой показатель отношения.
+        /// </summary>
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                if (value < 0)
+                {
+                    _value = 0;
+                    return;
+                }
+                else if (value > 100)
+                {
+                    _value = 100;
+                    return;
+                }
+
+                _value = value;
+            }
+        }
+
+        public Relation(NotNone<Colony> owner, NotNone<Colony> colony, int value)
+        {
+            Owner = owner;
+            Colony = colony;
+            Value = value;
+        }
+    }
+}
