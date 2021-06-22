ad615e4860adf2ed76e88a856d4a18743ede9288
EveryGameSPlay <repsbro@gmail.com>
2021-06-22 17:34:01 +0300

    Created deposit with test scene.

---
+++ Assets/_ProjectFiles/Code/Environment/Deposits/Deposit.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using Game.Entities.Resources;
+using UnityEngine;
+
+namespace Game.Environment.Deposits
+{
+    public class Deposit : MonoBehaviour
+    {
+        [SerializeField] private ResourceRarity resourceRarity;
+        
+        [SerializeField] private float spawnTime;
+        [SerializeField] private Transform spawnPlace;
+        
+        private IEnumerator spawnTimerRoutine;
+    }
+}
