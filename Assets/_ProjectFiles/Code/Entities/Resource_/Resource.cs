6c4413053ecb569bcddfec27cbe1357284f71e15
EveryGameSPlay <repsbro@gmail.com>
2021-06-23 00:31:59 +0300

    Deposit and Resource.

--- Assets/_ProjectFiles/Code/Entities/Resource_/Resource.cs
+++ Assets/_ProjectFiles/Code/Entities/Resource_/Resource.cs
@@ -1,24 +1,77 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using Game.Extensions;
+using UnityEngine;
 
 namespace Game.Entities.Resources
 {
     /// <summary>
     /// Базовый класс для всех ресурсов.
     /// </summary>
-    public class Resource : MonoBehaviour, IPickable
+    [TerminateFormat(TerminateFormat.Deactivate)]
+    public class Resource : LifetimeMonoBehaviour, IPickable
     {
         [SerializeField] private ResourceRarity _rarity;
+        [Tooltip("Время, по истечению которого, ресурс будет уничтожен, если он лежит.")]
+        [SerializeField] private float terminationTime;
 
+        private bool _terminate;
+        private float _timeToTermination;
+        
         public ResourceRarity Rarity => _rarity;
 
         public bool CanBePicked { get; private set; } = true;
-        
+
+        private void Update()
+        {
+            if (_terminate)
+            {
+                _timeToTermination -= Time.deltaTime;
+
+                if (_timeToTermination < 0)
+                {
+                    Terminate();
+                }
+            }
+        }
+
         public void Picked()
         {
+            StopTermination();
+            Lock();
         }
 
         public void Put()
         {
+            StartTermination();
+            Unlock();
+        }
+        
+        /// <summary>
+        /// Заблокировать возможность поднятия.
+        /// </summary>
+        public void Lock()
+        {
+            CanBePicked = false;
+        }
+
+        /// <summary>
+        /// Разблокировать возможность поднятия.
+        /// </summary>
+        public void Unlock()
+        {
+            CanBePicked = true;
+        }
+
+        private void StartTermination()
+        {
+            _terminate = true;
+            _timeToTermination = terminationTime;
+        }
+
+        private void StopTermination()
+        {
+            _terminate = false;
         }
     }
 }
--- Assets/_ProjectFiles/Code/Environment/Deposits/Deposit.cs
+++ Assets/_ProjectFiles/Code/Environment/Deposits/Deposit.cs
@@ -1,16 +1,76 @@
 ﻿using System.Collections;
+using Egsp.Core;
 using Game.Entities.Resources;
+using Game.Extensions;
 using UnityEngine;
 
 namespace Game.Environment.Deposits
 {
-    public class Deposit : MonoBehaviour
+    public class Deposit : MonoBehaviour, IPrefabUser
     {
         [SerializeField] private ResourceRarity resourceRarity;
         
         [SerializeField] private float spawnTime;
         [SerializeField] private Transform spawnPlace;
         
-        private IEnumerator spawnTimerRoutine;
+        private IEnumerator _spawnResourceRoutine;
+        
+        private Option<Resource> _resource  = Option<Resource>.None; 
+        
+        public IPrefabProvider PrefabProvider { get; set; }
+
+        private void Awake()
+        {
+            Spawn();
+        }
+
+        public Option<Resource> PickResource()
+        {
+            var resource = _resource;
+            _resource = Option<Resource>.None;
+            
+            if(_resource.IsNone)
+                Spawn();
+            
+            return resource;
+        }
+
+        private void Spawn()
+        {
+            if (_spawnResourceRoutine != null)
+                return;
+
+            _spawnResourceRoutine = SpawnResource();
+            StartCoroutine(_spawnResourceRoutine);
+        }
+
+        private IEnumerator SpawnResource()
+        {
+            Debug.Log("Spawn start.");
+            if (_resource.IsSome)
+            {
+                Debug.Log("Resource already spawned.");
+                _spawnResourceRoutine = null;
+                yield break;
+            }
+            
+            yield return new WaitForSeconds(spawnTime);
+
+            Debug.Log("Get resource from provider.");
+            
+            var resource = PrefabProvider.GetPrefab<Resource>(
+                x => x.Rarity == resourceRarity);
+
+            if (resource.IsNone)
+            {
+                Debug.Log($"{gameObject.name} Депозит не смог заспавнить ресурс. Не найдено подходящего префаба.");
+            }
+            else
+            {
+                _resource = resource;
+                _resource.Value.transform.position = spawnPlace.position;
+                _spawnResourceRoutine = null;
+            }
+        }
     }
 }
