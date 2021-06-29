63c0be8ecc5f53da0a6edff23e5a4608a726ed63
EveryGameSPlay <repsbro@gmail.com>
2021-06-29 10:38:11 +0300

    Added damage and health.

---
+++ Assets/_ProjectFiles/Code/Entities/General/Types/Damage.cs
@@ -0,0 +1,21 @@
+﻿namespace Game.Entities
+{
+    /// <summary>
+    /// Значение урона.
+    /// </summary>
+    public readonly struct Damage
+    {
+        public readonly uint Value;
+
+        public Damage(uint value)
+        {
+            Value = value;
+        }
+        
+        public static implicit operator uint(Damage damage)
+            => damage.Value;
+
+        public static explicit operator Damage(uint damage)
+            => new Damage(damage);
+    }
+}
---
+++ Assets/_ProjectFiles/Code/Entities/General/Types/Health.cs
@@ -0,0 +1,21 @@
+﻿namespace Game.Entities
+{
+    /// <summary>
+    /// Значение здоровья сущности.
+    /// </summary>
+    public readonly struct Health
+    {
+        public readonly uint Value;
+
+        public Health(uint value)
+        {
+            Value = value;
+        }
+
+        public static implicit operator uint(Health health)
+            => health.Value;
+
+        public static explicit operator Health(uint health)
+            => new Health(health);
+    }
+}
