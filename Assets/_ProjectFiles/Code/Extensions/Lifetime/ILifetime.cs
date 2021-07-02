e9b6fa7e8405f8254295fa16fa4e5c704f488384
EveryGameSPlay <repsbro@gmail.com>
2021-07-02 23:35:24 +0300

    Improved LifetimeMonoBehaviour.

--- Assets/_ProjectFiles/Code/Extensions/Lifetime/ILifetime.cs
+++ Assets/_ProjectFiles/Code/Extensions/Lifetime/ILifetime.cs
@@ -3,6 +3,9 @@ using JetBrains.Lifetimes;
 
 namespace Game.Extensions
 {
+    // Некоторые моменты будут повторять функционал Lifetime, однако они нужны в собственной реализации,
+    // т.к. логика отличается от стандартных объектов.
+    
     public interface ILifetime
     {
         Lifetime Lifetime { get; }
@@ -11,5 +14,14 @@ namespace Game.Extensions
         /// Способ, которым будет завершен жизненный цикл объекта.
         /// </summary>
         TerminateFormat Format { get; }
+        
+        /// <summary>
+        /// Терминирован ли объект.
+        /// </summary>
+        bool IsTerminated { get; }
+
+        void Parent(Lifetime parentLifetime);
+
+        void ChangeTerminateFormat(TerminateFormat format);
     }
 }
--- Assets/_ProjectFiles/Code/Extensions/Lifetime/LifetimeMonoBehaviour.cs
+++ Assets/_ProjectFiles/Code/Extensions/Lifetime/LifetimeMonoBehaviour.cs
@@ -5,18 +5,28 @@ using UnityEngine;
 
 namespace Game.Extensions
 {
+    /// <summary>
+    /// В методе Awake и Start
+    /// </summary>
     public abstract class LifetimeMonoBehaviour : MonoBehaviour, ILifetime
     {
         /// <summary>
-        /// Ключ к управлению времени жизни объекта.
+        /// Родительский лайфтайм, который может быть установлен извне.
+        /// Нужен чтобы привязать данный объект к другому. 
+        /// </summary>
+        private Option<Lifetime> _parentLifetime = Option<Lifetime>.None; 
+        
+        /// <summary>
+        /// Ключ к управлению времени жизни объекта. Именно на этот лайфтайм привязываются
+        /// другие объекты.
         /// </summary>
         private LifetimeDefinition _lifetimeDefinition = new LifetimeDefinition();
 
         // Изначально формат ничему не равен. Позже он будет определен с помощью атрибута или вручную.
-        protected Option<TerminateFormat> _format = Option<TerminateFormat>.None;
+        private Option<TerminateFormat> _format = Option<TerminateFormat>.None;
 
         public Lifetime Lifetime => _lifetimeDefinition != null ?
-            _lifetimeDefinition.Lifetime : (_lifetimeDefinition = new LifetimeDefinition()).Lifetime;
+            _lifetimeDefinition.Lifetime : Alive().Lifetime;
 
         public TerminateFormat Format
         {
@@ -34,20 +44,28 @@ namespace Game.Extensions
             set => _format = value;
         }
 
+        public bool IsTerminated => _lifetimeDefinition == null;
+
         /// <summary>
         /// Завершает жизнь объекта.
         /// Он будет уничтожен или отключен для дальнейшего использования в зависимости от настроек.
         /// </summary>
         protected void Terminate()
         {
-            if (_lifetimeDefinition == null)
+            if (IsTerminated)
                 return;
             
             // Если объект живой.
             if (_lifetimeDefinition.Status == LifetimeStatus.Alive)
             {
+                // Терминируем все дочерние лайфтаймы.
                 _lifetimeDefinition.Terminate();
+                // Очищаем ссылку, т.к. позже мы обновим лайфтайм.
                 _lifetimeDefinition = null;
+                
+                // Избавляемся от родительского лайфтайма, чтобы в дальнейшем можно было установить 
+                // нового родителя, если наш объект не будет уничтожаться.
+                _parentLifetime = Option<Lifetime>.None;
 
                 switch (Format)
                 {
@@ -75,6 +93,54 @@ namespace Game.Extensions
             {
                 return formatAttribute.Format;   
             }
+        }
+
+        /// <summary>
+        /// Возобновляет лайфтайм объекта, если это возможно.
+        /// Возвращает ссылку на уже обновленный лайфтайм.
+        /// </summary>
+        protected LifetimeDefinition Alive()
+        {
+            _lifetimeDefinition = new LifetimeDefinition();
+            
+            OnAlive();
+            
+            return _lifetimeDefinition;
+        }
+        
+        /// <summary>
+        /// Вызывается после восстановления лайфтайма.
+        /// </summary>
+        protected virtual void OnAlive(){}
+
+        /// <summary>
+        /// Устанавливает для объекта родительский лайфтайм.
+        /// Метод появился по причине недоступности конструктора объекта MonoBehaviour.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">Нельзя установить родительский лайфтайм, если он был установлен ранее.</exception>
+        public void Parent(Lifetime parentLifetime)
+        {
+            if (_parentLifetime.IsSome)
+                throw new InvalidOperationException("Нельзя установить родительский лайфтайм, если он был установлен ранее.");
+            
+            // Создаем лайфтайм, связанный с родительским.
+            var parentedLifetimeDefinition = parentLifetime.CreateNested();
+            // Нам дефинишн не нужен, потому что у нас есть свой собственный.
+            // Можно было бы заменить свой на новый, однако в момент создания объекта,
+            // Awake или Start на наш лайфтайм может кто-нибудь подписаться.
+            // Можно было бы сделать рекомендацию, но ее же никто не будет соблюдать :).
+            // Поэтому контроллируем два лайфтайма.
+            _parentLifetime = parentedLifetimeDefinition.Lifetime;
+            // Терминируем наш объект, если родительский объект начал терминацию.
+            _parentLifetime.Value.OnTermination(Terminate);
+        }
+
+        /// <summary>
+        /// Изменяет формат терминации.
+        /// </summary>
+        public void ChangeTerminateFormat(TerminateFormat format)
+        {
+            _format = format;
         }
     }
 }
